c7a6a6ae1d49da37e78c278f80e909d64c11839d
kailastr <mailtokailastr@gmail.com>
2024-06-29 16:23:43 +0530

    Get Message

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -29,15 +29,42 @@ export const sendMessage = async (req, res) => {
 
         if (newMessage) {
             conversation.messages.push(newMessage._id);
-            // await conversation.save();
-            // await newMessage.save();    --- instead of using .save function for multiple data we could say
-            await Promise.all([conversation.save(), newMessage.save()]); // in this way both the .save functions run in parallel
         }
+
+        //SOCKET IO functionalities
+
+        // await conversation.save();
+        // await newMessage.save();    --- instead of using .save function for multiple data we could say
+        await Promise.all([conversation.save(), newMessage.save()]); // in this way both the .save functions run in parallel
 
         res.status(201).json(newMessage);
 
     } catch (error) {
         console.log("SendMessage Controller Error : ", error.message);
         res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+
+        const { id: userToChatId } = req.params;
+        const { _id: senderId } = req.user;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] }
+        }).populate("messages"); //Conversation.findOne gives us the id of the messages. The .populate function help us to get the messages based on their id
+
+        if (!conversation) {
+            return res.status(200).json([]);
+        }
+
+        const messages = conversation.messages;
+
+        return res.status(200).json(messages);
+
+    } catch (error) {
+        console.log("getMessage Controller error : ", error.message);
+        return res.status(500).json({ error: "Internal Server Error !!" });
     }
 }
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { sendMessage } from '../controllers/message.controller.js';
+import { getMessages, sendMessage } from '../controllers/message.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
+//route for sending messages
 router.post('/send/:id', protectRoute, sendMessage); //adding a middleware function to ensure that the route is verified
+
+//route for getting messages
+router.get('/:id', protectRoute, getMessages);
 
 export default router;
