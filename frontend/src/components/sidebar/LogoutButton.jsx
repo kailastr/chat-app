09d51cdedb505a585702ed9766a704d8a8339ff9
kailastr <mailtokailastr@gmail.com>
2024-07-08 17:35:13 +0530

    Logout Hook

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { BiLogOut } from "react-icons/bi";
+import useLogout from '../../hooks/useLogout.js';
 
 const LogoutButton = () => {
+    const { loading, logout } = useLogout();
     return (
         <div className=' mt-auto pt-2'>
-            <span className='flex items-center gap-2 text-white cursor-pointer border p-2 rounded-lg hover:bg-gray-500 hover:bg-opacity-70 transition duration-200 ease-in-out'>
-                <BiLogOut className='w-6 h-6' /> Logout
-            </span>
+            {!loading ?
+                (
+                    <span
+                        className='flex items-center gap-2 text-white cursor-pointer border p-2 rounded-lg hover:bg-gray-500 hover:bg-opacity-70 transition duration-200 ease-in-out'
+                        onClick={logout}
+                    >
+                        <BiLogOut className='w-6 h-6' /> Logout
+                    </span>
+                )
+                :
+                (
+                    <div
+                        className='flex items-center gap-2 text-white cursor-pointer border p-2 rounded-lg hover:bg-gray-500 hover:bg-opacity-70 transition duration-200 ease-in-out'
+                    >
+                        <span className='loading loading-spinner'></span>
+                    </div>
+                )
+            }
         </div>
     )
 }
