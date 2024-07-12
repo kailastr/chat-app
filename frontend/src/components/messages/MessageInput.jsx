83dce3ee92488a126ddd0dbcdad7445db491c3fd
kailastr <mailtokailastr@gmail.com>
2024-07-12 15:27:25 +0530

    Sending Messages

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSend } from "react-icons/io5";
+import useSendMessage from '../../hooks/useSendMessage.js';
 
 const MessageInput = () => {
+
+    const [message, setMessage] = useState("");
+
+    const { loading, sendMessage } = useSendMessage();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!message) return;
+
+        await sendMessage(message);
+
+        setMessage("");
+    }
+
     return (
-        <form className='px-4 py-3 '>
+        <form
+            className='px-4 py-3 '
+            onSubmit={handleSubmit}
+        >
             <div className='w-full relative'>
-                <input type="text"
-                    name=""
-                    id=""
+                <input
+                    type="text"
                     placeholder='Write Message'
                     className='border border-gray-600 text-sm rounded-lg block w-full p-2.5 bg-gray-700 text-white'
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
                 />
                 <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
-                    <IoSend />
+                    {loading ? <span className='loading loading-spinner' /> : <IoSend />}
                 </button>
             </div>
         </form>
